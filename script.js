7ff9f8578b7ff2e10c28e0370f66cff7c80120e8
sanjeevks149 <sanjeevks149@gmail.com>
2025-03-24 11:13:57 +0530

    Added PlayGame function

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
-let computerchoice ="";
-let humanchoice;
-let humanScore  = 0;
-let computerScore = 0;
 function getComputerChoice(){
-    randonchoice = Math.round(Math.random() *100)
+    let computerchoice;
+    let randonchoice = Math.round(Math.random() *100)
     console.log(randonchoice)
     if(randonchoice<=33)
         computerchoice = "Rock";
@@ -24,19 +21,33 @@ function getHumanChoice(){
         humanchoice = "Scissor";
     return humanchoice.toUpperCase();
 }
-function playRound(humanchoice,computerchoice){
-    if(humanchoice==computerchoice){
-        humanScore++;
-        console.log("You Win! ",humanchoice," beats ",computerchoice);
-    }
-    else{
-        console.log("You Lose! ",computerchoice," beats ",humanchoice);
-        computerScore++;
-    }
-}
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
 
-playRound(humanSelection, computerSelection);
-getComputerChoice();
-getHumanChoice();
+
+
+function Playgame(){
+    let humanScore  = 0;
+    let computerScore = 0;
+    function playRound(humanchoice,computerchoice){    
+        if(humanchoice==computerchoice){
+            humanScore++;
+            console.log("You Win! ",humanchoice," beats ",computerchoice);
+        }
+        else{
+            console.log("You Lose! ",computerchoice," beats ",humanchoice);
+            computerScore++;
+        }
+    }
+    let round = 0;
+    while(round < 5){
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+        playRound(humanSelection,computerSelection);
+        round++;
+    }
+    if(humanScore>computerScore)
+        console.log("You WIN")
+    else
+        console.log("You lose")
+    
+}
+Playgame();
